9fdcbe43e7616c1e3149f7fd23868649bd27889c
maxence TISSOT <maxence.tissot@live.fr>
2020-03-27 11:31:12 +0100

    start

---
+++ src/main/java/com/hub/ems/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.hub.ems.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hub.ems.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
